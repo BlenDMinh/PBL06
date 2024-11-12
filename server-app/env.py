89cbb79652b55f36f75ef524bd8e6c220d2da074
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-11-12 10:16:53 +0700

    hotfix: Update env.py

--- server-app/env.py
+++ server-app/env.py
@@ -1,11 +1,52 @@
+import logging
 from dotenv import dotenv_values
+
+env_keys = [
+    # Host config
+    "MYSQL_DB_HOST",
+    "MYSQL_DB_PORT",
+    "SERVER_APP_HOST",
+    "SERVER_APP_PORT",
+    "SERVER_AI_HOST",
+    "SERVER_AI_PORT",
+    "APP_ENV",
+
+    # MySQL Database env
+    "MYSQL_USER",
+    "MYSQL_PASSWORD",
+    "MYSQL_DATABASE",
+    "DATABASE_URL",
+
+    # Nextjs env
+    "LOCAL_API_URL",
+    "JWT_SECRET",
+    "PING_INTERVAL",
+    "ACCESS_TOKEN_EXPI",
+    "REFRESH_TOKEN_EXPI",
+
+    # FastAPI AI env
+    "GIT_MODEL_NAME",
+    "GIT_MODEL_PATH",
+    "GIT_PARAM_PATH",
+    "SERVER_AI_DATABASE_ECHO",
+
+    # For Hugging-face
+    "HF_HOME",
+
+    # FastAPI Server env
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
+]
 
 config = dotenv_values(".env")
 
 import os
 
 # Override .env values with OS environment variables if they exist
-config = {key: os.getenv(key, default=value) for key, value in config.items()}
+config = {key: os.getenv(key, default=config.get(key, "")) for key in env_keys}
+
+logging.info(f"Config: {config}")
 
 if __name__ == "__main__":
     # Clear all environment variables that exists in .env
@@ -17,6 +58,6 @@ if __name__ == "__main__":
     import os
 
     # Override .env values with OS environment variables if they exist
-    config = {key: os.getenv(key, default=value) for key, value in config.items()}
+    config = {key: os.getenv(key, default=config.get(key, "")) for key in env_keys}
     
     print(config)
