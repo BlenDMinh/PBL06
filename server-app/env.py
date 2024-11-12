92b8b16143e777b71a437b9f65ba8c6164ae9781
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-11-12 10:04:53 +0700

    hotfix: load environments properly env.py

--- server-app/env.py
+++ server-app/env.py
@@ -1,3 +1,22 @@
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
+
+import os
+
+# Override .env values with OS environment variables if they exist
+config = {key: os.getenv(key, default=value) for key, value in config.items()}
+
+if __name__ == "__main__":
+    # Clear all environment variables that exists in .env
+    for key in config.keys():
+        os.environ.pop(key, None)
+
+    config = dotenv_values(".env")
+
+    import os
+
+    # Override .env values with OS environment variables if they exist
+    config = {key: os.getenv(key, default=value) for key, value in config.items()}
+    
+    print(config)
