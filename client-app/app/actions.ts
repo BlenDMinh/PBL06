7aa9e8079b1298406d87fe13daf48b4e9fc3f9b0
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-11-16 16:00:51 +0700

    Remove prisma from Dockerfile
    docker 2

--- client-app/app/actions.ts
+++ client-app/app/actions.ts
@@ -2,11 +2,9 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import api from "@/lib/util/api";
 export async function handleLogout() {
   console.log("Logging out");
   cookies().delete("access_token");
   cookies().delete("refresh_token");
   redirect("/auth/login");
 }
-
--- client-app/app/auth/register/action.tsx
+++ client-app/app/auth/register/action.tsx
@@ -1,11 +1,20 @@
 "use server";
 
 import RegisterResponseSchema from "@/lib/schema/auth/register_response.schema";
-import api from "@/lib/util/api";
+import { getServerAppAxio } from "@/lib/util/api";
 
-export async function serverSideRegister(email: string, password: string, username: string) {
+export async function serverSideRegister(
+  email: string,
+  password: string,
+  username: string
+) {
   try {
-    const response = await api.post("/auth/register", { email, password, username });
+    const api = getServerAppAxio();
+    const response = await api.post("/auth/register", {
+      email,
+      password,
+      username,
+    });
     const data = RegisterResponseSchema.parse(response.data);
     return data;
   } catch (error) {
